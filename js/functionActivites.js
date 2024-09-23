52dc4b1662dbd40ce770e10caa55cf1d1f0ec646
pkcbcse <pradipkcb.cse@gmail.com>
2024-09-24 02:51:24 +0600

    11th commit-Related js file added

---
+++ js/functionActivites.js
@@ -0,0 +1,100 @@
+function getElementById(eId) {
+  return document.getElementById(eId);
+}
+
+function GetElementValueById(id) {
+  return document.getElementById(id).value;
+}
+
+function donationCalculate() {
+  const myTotalMoney = parseInt(getElementById('myTotalMoney').innerText);
+  return myTotalMoney;
+}
+
+function historyCreate(dTk, toDestination) {
+  const historyTab = getElementById('historyTab');
+  const Time = new Date();
+  const div = document.createElement('div');
+  div.classList.add(
+    'border',
+    'w-full',
+    'rounded-lg',
+    'shadow-lg',
+    'p-5',
+    'mt-10'
+  );
+  div.innerHTML = `
+        <p class = "text-base md:text-2xl font-bold"><span>${dTk}</span> is Donated For ${toDestination}</p>
+        <p class = "md:font-semibold">Date : ${Time}</p>
+    `;
+
+  // historyTab.appendChild(div)
+  historyTab.insertBefore(div, historyTab.childNodes[0]);
+}
+
+// setTimeOut for PopUp Modal 10 Second
+
+function popUpTimeout() {
+  setTimeout(() => {
+    successPop.classList.add('hidden');
+  }, 10000);
+}
+
+function calculate(dTitle, inputV, Totaldiv) {
+  const donationTitle = getElementById(dTitle).innerText;
+  const inputValue = getElementById(inputV);
+  const donationInputValue = GetElementValueById(inputV);
+  const totalDonationAmount = parseInt(getElementById(Totaldiv).innerText);
+  const donationValueInt = parseInt(donationInputValue);
+
+  if (isNaN(donationInputValue) || donationInputValue < 0) {
+    alert('Please Enter a Valid Number');
+    return;
+  }
+
+  if (donationCalculate() > donationValueInt) {
+    const nResult = totalDonationAmount + donationValueInt;
+    getElementById(Totaldiv).innerHTML = nResult;
+
+    const lastMoney = donationCalculate() - donationValueInt;
+    getElementById('myTotalMoney').innerText = lastMoney;
+
+    historyCreate(donationValueInt, donationTitle);
+
+    successPop.classList.remove('hidden');
+    popUpTimeout();
+
+    inputValue.value = '';
+  } else {
+    alert('insufficient Balance');
+  }
+}
+
+// const titleNoakhali = getElementById('titleNoakhali').innerText;
+// const noakhaliDonationInputValue = getElementById('noakhaliInput');
+// const noakhaliDonationAmount = parseInt(getElementById('noakhaliDonationAmount').innerText);
+// const noakhaliInput = parseInt(GetElementValueById('noakhaliInput'));
+
+// if ( isNaN(noakhaliDonationInputValue.value) || noakhaliDonationInputValue.value<0) {
+//     alert("Please Enter a Valid Number")
+//     return
+// }
+
+// if (donationCalculate()>noakhaliInput) {
+
+//     const nResult = noakhaliDonationAmount + noakhaliInput;
+//     getElementById('noakhaliDonationAmount').innerHTML = nResult
+
+//     const lastMoney = donationCalculate() - noakhaliInput;
+//     getElementById('myTotalMoney').innerText = lastMoney;
+
+//     historyCreate(noakhaliInput, titleNoakhali);
+
+//     successPop.classList.remove('hidden')
+//     popUpTimeout()
+//     noakhaliDonationInputValue.value = '';
+
+// }
+// else{
+//     alert("insufficient Balance")
+// }
---
+++ js/jsForIndex.js
@@ -0,0 +1,75 @@
+const historyTabBtn = getElementById('historyTabBtn');
+const donationTabBtn = getElementById('donationTabBtn');
+const donationTab = getElementById('donationTab');
+const historyTab = getElementById('historyTab');
+const successPop = getElementById('successPop');
+const popup = document.getElementById('popup');
+const btnNoakhali = document.getElementById('btnNoakhali');
+const btnFeni = document.getElementById('btnFeni');
+const btnQuota = document.getElementById('btnQuota');
+
+// Donation Tab Button Work Here
+donationTabBtn.addEventListener('click', function () {
+  donationTabBtn.classList.add('bg-primary');
+  donationTabBtn.classList.remove('bg-white');
+
+  historyTabBtn.classList.remove('bg-primary');
+  historyTabBtn.classList.add('bg-white');
+
+  // unhide Donation Tab Element
+  donationTab.classList.remove('hidden');
+
+  // hide history Tab
+  historyTab.classList.add('hidden');
+});
+
+// History Tab Button Work Here
+
+historyTabBtn.addEventListener('click', function () {
+  // Change button color
+  historyTabBtn.classList.add('bg-primary');
+  historyTabBtn.classList.remove('bg-white');
+
+  donationTabBtn.classList.remove('bg-primary');
+  donationTabBtn.classList.add('bg-white');
+
+  // hide Donation Tab Element
+  donationTab.classList.add('hidden');
+
+  // unhide history Tab
+  historyTab.classList.remove('hidden');
+});
+
+// Calculate Button Work Here
+
+btnNoakhali.addEventListener('click', function () {
+  const dTitle = 'titleNoakhali';
+  const inputV = 'noakhaliInput';
+  const Totaldiv = 'noakhaliDonationAmount';
+
+  calculate(dTitle, inputV, Totaldiv);
+});
+
+// Feni Calculate Btn Work Here
+btnFeni.addEventListener('click', function () {
+  const dTitle = 'feniTitle';
+  const inputV = 'feniInput';
+  const Totaldiv = 'feniDonationAmmount';
+
+  calculate(dTitle, inputV, Totaldiv);
+});
+
+// Quota Calculate Btn Work Here
+btnQuota.addEventListener('click', function () {
+  const dTitle = 'quotaTitle';
+  const inputV = 'quotaInput';
+  const Totaldiv = 'quotaDonationAmmount';
+
+  calculate(dTitle, inputV, Totaldiv);
+});
+
+// Close Pop Up
+
+getElementById('closePop').addEventListener('click', function () {
+  successPop.classList.add('hidden');
+});
